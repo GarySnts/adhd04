e2bf2e7d53a697eca364f9a50291c7c111f5667c
GarySnts <gsami.santos@hotmail.com>
2024-06-09 23:55:15 -0600

    primera version

---
+++ dist/solucion01.js
@@ -0,0 +1,5 @@
+"use strict";
+const inputNumero1 = document.getElementById('txtNumero1');
+const inputNumero2 = document.getElementById('txtNumero2');
+const inputNumero3 = document.getElementById('txtNumero3');
+const verResultado = document.getElementById('resultado');
---
+++ src/solucion01.ts
@@ -0,0 +1,11 @@
+const inputNumero1 = document.getElementById('txtNumero1') as HTMLInputElement;
+const inputNumero2 = document.getElementById('txtNumero2') as HTMLInputElement;
+const inputNumero3 = document.getElementById('txtNumero3') as HTMLInputElement;
+
+
+
+
+
+
+
+const verResultado = document.getElementById('resultado') as HTMLParagraphElement;
